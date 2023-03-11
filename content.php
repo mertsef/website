bfb747392e9922a98fbcf64b5d218ee86b558ccf
void() <116299032+mertsef@users.noreply.github.com>
2023-03-11 21:15:17 +0100

    added existing files .css, .php

---
+++ content.php
@@ -0,0 +1,22 @@
+<?php
+           $headline = "Test Webseite";
+           $test=[];
+            echo "<h1>" . $headline . "</h1>";
+                $test="test";
+            if ($_GET["page"]=="Start")
+            {
+                echo "sieht man das?";
+            }
+            if ($_GET["page"]=="Menü")
+            {
+                echo "das sollte hoffentlich das Menü sein?";
+            }
+            if ($_GET["page"]=="Auswahl")
+            {
+                echo "Auswahl: ";
+            }
+            if ($_GET["page"]=="Info")
+            {
+                echo "";
+            }
+            ?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <div class="menubar">
+            <h1>sieht man das?</h1>
+            <div>
+                <div class="me">1.
+            </div>
+                <div class="menu">
+                <a href="index.php?page=Start">Start</a> |
+                <a href="index.php?page=Menü">Menü</a> |
+                <a href="index.php?page=Auswahl">Auswahl</a> |
+                <a href="index.php?page=Info">Info</a>
+            </div>
+    <div class="content">
+            <?php include __DIR__."/content.php" ?>
+</div>
+            <div class="footer">
+                <div class="footertext">
+                TEST FOOTER    
+                <?php include __DIR__."/footer.php" ?>
+</div>
+</div>
+        </body>
+</html>
